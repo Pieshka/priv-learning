06289c2cfa4550ec32e33fa48f8e795e4d04772a
Pieszka <39170443+Pieshka@users.noreply.github.com>
2025-10-06 15:57:56 +0200

    change: Lotto.java task, add: .gitignore and Lab1.iml

--- Lab1/Lotto.java
+++ Lab1/Lotto.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Random;
 
-public class Lotto {
+public class Lotto
+{
+    public static void main(String[] args)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        Random rand = new Random();
+        for (int i = 0; i < 6; i++)
+        {
+            set.add(rand.nextInt(1,50));
+        }
+        System.out.println(set);
+    }
 }
