e784ba0d3bc009de294f53f721b181d8901922b6
Pieszka <39170443+Pieshka@users.noreply.github.com>
2025-10-13 17:07:36 +0200

    change: lab2: extend Lotto

--- Lab2/Lotto.java
+++ Lab2/Lotto.java
@@ -3,9 +3,17 @@ import java.util.ArrayList;
 
 public class Lotto
 {
-    public static void main()
+    public static void main(String[] args)
     {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        if(args.length < 6)
+        {
+            System.out.println("Usage: java Lotto <number> <number> <number> <number> <number> <number>");
+            System.exit(1);
+        }
+
+        ArrayList<Integer> userNumbers = new ArrayList<>();
+        for (String arg : args) userNumbers.add(Integer.parseInt(arg));
+        ArrayList<Integer> numbers = new ArrayList<>();
         Random rand = new Random();
         while(numbers.size() < 6)
         {
@@ -14,6 +22,13 @@ public class Lotto
                 numbers.add(nextRandom);
         }
 
-        System.out.println(numbers);
+        int countUserHits = 0;
+        for(int i = 0; i < userNumbers.size(); i++)
+            if(numbers.contains(userNumbers.get(i)))
+                countUserHits++;
+
+        System.out.println("Twoje typy: " + userNumbers);
+        System.out.println("Wylosowane liczby: " + numbers);
+        System.out.println("Liczba trafień: " + countUserHits);
     }
 }
